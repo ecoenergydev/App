3acd0d4367af56e799f861b7d719185503b167e7
ecoenergydev <ecoenergydeveloper@gmail.com>
2026-04-07 22:04:51 +0300

    Add files via upload

---
+++ hex-t-bot/src/main/java/com/dp/hex_t_bot/HexTBotApplication.java
@@ -0,0 +1,13 @@
+package com.dp.hex_t_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexTBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexTBotApplication.class, args);
+	}
+
+}
---
+++ hex-t-bot/src/test/java/com/dp/hex_t_bot/HexTBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dp.hex_t_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexTBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
