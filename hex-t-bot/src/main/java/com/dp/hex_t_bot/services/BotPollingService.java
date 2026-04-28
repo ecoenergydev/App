3fbacd5c5497925ccbf94acaf6535c9f2494111b
Gri <gri@gmail.com>
2026-04-28 22:30:41 +0300

    update

---
+++ hex-t-bot/src/main/java/com/dp/hex_t_bot/services/BotPollingService.java
@@ -0,0 +1,13 @@
+package com.dp.hex_t_bot.services;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BotPollingService {
+
+    @Scheduled
+    public void poll() {
+
+    }
+}
