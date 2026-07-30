bcf0aada41c878bac9723354d4a18c613e6ea38d
Gri <gri@gmail.com>
2026-07-30 21:43:13 +0300

    update RestTemplate

--- hex-t-bot/src/main/java/com/dp/hex_t_bot/config/TgClientConfig.java
+++ hex-t-bot/src/main/java/com/dp/hex_t_bot/config/TgClientConfig.java
@@ -7,11 +7,12 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.client.RestTemplateBuilder;
 
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 @Configuration
 public class TgClientConfig {
-    private static final String TG_BOT_API_URL = "https://api.telegram.org/bot%s";
-
+    private static final String TG_BOT_API_URL = "https://api.telegram.org/bot%s/";
+        // вместо %s будет указан токен из конфигурации (переменная среды)
     @Bean
     public RestTemplate tgClient(
             @Value("${tg.bot.timeout}") int timeout,
@@ -19,8 +20,8 @@ public class TgClientConfig {
             RestTemplateBuilder restTemplateBuilder
     ) {
         var timeoutDuration = Duration.of(timeout, ChronoUnit.SECONDS);
-        restTemplateBuilder
-                .rootUri(TG_BOT_API_URL.formatted(token))
+        return restTemplateBuilder
+                .rootUri(TG_BOT_API_URL.formatted(token)) // токен из конфигурации
                 .setReadTimeout(timeoutDuration)
                 .setConnectionTimeout(timeoutDuration)
                 .build();
